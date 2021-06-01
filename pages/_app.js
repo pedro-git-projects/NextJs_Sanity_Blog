38e1ce981f2e8d0d4b53fe2639b3b3a07aa3260b
Pedro Git Projects <pedro.coding.contact@gmail.com>
2021-06-01 02:35:35 -0300

    started using font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/index.scss';
 import 'highlight.js/styles/dracula.css';
 
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons';
+
+config.autoAddCss = false;
+library.add(faList, faBorderAll);
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
